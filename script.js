3d17de372674a9855a5d3e663d2f365f72b2dfbe
Hennc94 <h3nn3ssyc@yahoo.com>
2022-12-10 09:22:15 -0500

    Calc Project

--- script.js
+++ script.js
@@ -1,53 +1,79 @@
-//to select class display
-let display = document.querySelector("#display")
-//to select all buttons
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll('button')
+let display = document.querySelector('#display')
 
-let numText = display.innerText
+let firstNum = 0
+let secondNum = 0
+let operatorChosen = false
+let operator = ''
 
-for (const btn of buttons) {
-  btn.addEventListener("click", function (e) {
-    let btnText = e.target.innerText
-    if (btnText === "AC") {
-      display.innerText = ""
-    } else if (btnText === "÷") {
-      display.innerText += "/"
-    } else if (btnText === "*") {
-      display.innerText += "*"
-    } else if (btnText === "=") {
-    } else {
-      (display.innerText += btnText)
-    }
-  })
-
+function handleOperator(op){
+  
+  if(operatorChosen === false){
+    operator = op
+    firstNum = display.innerText
+    display.innerText = ''
+    operatorChosen = true
+  }
+  
 }
 
-// let numText = display.innerText
-// let splitText = numText.split("+")
-console.log(numText)
+function handleEvaluation(){
+  
+  operatorChosen = false;
+  secondNum = display.innerText
+  
+  if(operator === '+'){
+    return parseFloat(firstNum)+parseFloat(secondNum)
+  }
+  if(operator === '*'){
+    return parseFloat(firstNum)*parseFloat(secondNum)
+  }
+}
 
 
-// function sumNum(str){
-//   let strNum = str.split(",")
-//   let sum = display.reduce(function(total,num){
-//     return parseFloat(total) + parseFloat(num)
-//   })
-//   return sum
-// }
+for(let button of buttons){
+  button.addEventListener('click', e=>{
+    console.log(e.target.classList[0] == 'number')
+    let num = e.target.dataset.number
+    let op = e.target.dataset.operator
+    
+    if(op === '='){
+      display.innerText = handleEvaluation()
+      return;
+    }
+    
+    if(num){
+      display.innerText+=num;
+    }
+    if(op){
+      handleOperator(op)
+    }
+    
+  })
+}
+  
+
+
+// e.target.id
+// display.innerText.slice(0, -1)
+
+
+  
 
 
 
 
 
-//   document.addEventListener('click', e => {
-//   let coin = document.createElement('div')
-//   coin.classList.add('coin')
-//   coin.style.top = `${e.clientY}px`
-//   coin.style.left = `${e.clientX}px`
-//   document.body.append(coin)
-//   console.log(e.clientX, e.clientY)
 
-//   setTimeout(() => {
-//     coin.remove()
-//   }, 400)
-// })
+  document.addEventListener('click', e => {
+  let coin = document.createElement('div')
+  coin.classList.add('coin')
+  coin.style.top = `${e.clientY}px`
+  coin.style.left = `${e.clientX}px`
+  document.body.append(coin)
+  console.log(e.clientX, e.clientY)
+
+  setTimeout(() => {
+    coin.remove()
+  }, 400)
+})
