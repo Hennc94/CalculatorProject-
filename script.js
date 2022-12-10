ce41650837b86c905362842d8234156da6ec51ac
Hennc94 <h3nn3ssyc@yahoo.com>
2022-12-10 10:34:52 -0500

    Calc Proj

--- script.js
+++ script.js
@@ -6,71 +6,71 @@ let secondNum = 0
 let operatorChosen = false
 let operator = ''
 
-function handleOperator(op){
-  
-  if(operatorChosen === false){
+function handleOperator(op) {
+
+  if (operatorChosen === false) {
     operator = op
     firstNum = display.innerText
     display.innerText = ''
     operatorChosen = true
   }
-  
+
 }
 
-function handleEvaluation(){
-  
+function handleEvaluation() {
+
   operatorChosen = false;
   secondNum = display.innerText
+
+  if (operator === '+') {
+    return parseFloat(firstNum) + parseFloat(secondNum)
+  }
+  if (operator === '*') {
+    return parseFloat(firstNum) * parseFloat(secondNum)
+  }
+  if (operator === "-") {
+    return parseFloat(firstNum) - parseFloat(secondNum)
+  }
+  if (operator ==="÷") {
+    return parseFloat(firstNum) / parseFloat(secondNum)
+  }
   
-  if(operator === '+'){
-    return parseFloat(firstNum)+parseFloat(secondNum)
-  }
-  if(operator === '*'){
-    return parseFloat(firstNum)*parseFloat(secondNum)
-  }
 }
 
 
-for(let button of buttons){
-  button.addEventListener('click', e=>{
+for (let button of buttons) {
+  button.addEventListener('click', e => {
     console.log(e.target.classList[0] == 'number')
     let num = e.target.dataset.number
     let op = e.target.dataset.operator
-    
-    if(op === '='){
+
+    if (op === '=') {
       display.innerText = handleEvaluation()
       return;
     }
-    
-    if(num){
-      display.innerText+=num;
+
+    if (num) {
+      display.innerText += num;
     }
-    if(op){
+    if (op) {
       handleOperator(op)
     }
-    
+
   })
 }
-  
 
 
+//don't forget backspace button
 // e.target.id
 // display.innerText.slice(0, -1)
 
-
-  
-
-
-
-
-
-
-  document.addEventListener('click', e => {
+document.addEventListener('click', e => {
   let coin = document.createElement('div')
   coin.classList.add('coin')
   coin.style.top = `${e.clientY}px`
   coin.style.left = `${e.clientX}px`
   document.body.append(coin)
+  
   console.log(e.clientX, e.clientY)
 
   setTimeout(() => {
